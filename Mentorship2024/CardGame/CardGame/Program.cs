cae68801738ca29431e4bcc4e17750e2389d7ea4
duc20062001@gmail.com <85180552+ducvuive@users.noreply.github.com>
2024-09-23 00:02:50 +0700

    no message

---
+++ Mentorship2024/CardGame/CardGame/Program.cs
@@ -0,0 +1,31 @@
+﻿using CardGame.Class;
+using System;
+using static CardGame.Class.PlayerClass;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the Console Baccarat Game!");
+        GameClass game = new GameClass();
+        game.DealInitialCards();
+
+        DisplayPlayerHands(game);
+
+        Console.ReadKey();
+    }
+
+    static void DisplayPlayerHands(GameClass game)
+    {
+        int playerNumber = 1;
+        foreach (var player in game.players)
+        {
+            Console.WriteLine($"Player {playerNumber++}'s hand:");
+            foreach (var card in player.Hand)
+            {
+                Console.WriteLine($"- {card.Value} of {card.Suit}");
+            }
+            Console.WriteLine($"Score: {player.CalculateScore()}\n");
+        }
+    }
+}
---
+++ Mentorship2024/RomanToInteger/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using RomanToInteger;
+class Program
+{
+    static void Main()
+    {
+        // Get input from the user
+        Console.Write("Enter a Roman numeral to convert to an integer: ");
+        string roman = Console.ReadLine().ToUpper();
+        ActionConvert converter = new ActionConvert();
+        int number = converter.RomanToInteger(roman);
+        if (number != -1)
+        {
+            Console.WriteLine($"The integer value of {roman} is {number}");
+        }
+        else
+        {
+            Console.WriteLine("Invalid Roman numeral.");
+        }
+    }
+}
